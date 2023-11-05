3e64cc530a51509b4fb27fa48751df24e7eb7381
Nazim <yasarnazimsari@gmail.com>
2023-11-05 15:34:32 +0300

    cleanBlogOdev

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
 
+//template engin
+app.set("view engine", "ejs");
+
+//middleWare
+
+app.use(express.static("public"));
+
+//routes
+
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  // res.sendFile(path.resolve(__dirname, "views/index.html"));
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 3000;
