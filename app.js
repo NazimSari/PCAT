2cdf0f020e3742001b649529c975d9e44b58ac56
Nazim <yasarnazimsari@gmail.com>
2024-01-15 00:15:16 +0300

    update işlemleri yapıldı

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
 const Post = require("./models/Post");
+const methodOverride = require("method-override");
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 //routes
 
@@ -44,6 +46,21 @@ app.get("/about", (req, res) => {
 
 app.get("/add_post", (req, res) => {
   res.render("add_post");
+});
+app.get("/posts/edit_post/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render("edit_post", {
+    post,
+  });
+});
+
+app.put("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.save();
+
+  res.redirect(`/posts/${req.params.id}`);
 });
 
 app.post("/posts", async (req, res) => {
